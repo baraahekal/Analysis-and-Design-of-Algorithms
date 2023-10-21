47e856ff1e8d9b5ed15948ed0c77af9d2b944daa
baraahekal <ibaraahekal@gmail.com>
2023-10-21 11:25:28 +0300

    fixed an issue related to list size

--- get_intersection.py
+++ get_intersection.py
@@ -1,9 +1,8 @@
 def get_intersection(lst1, lst2):
     intersection = []
 
-    n, m = len(lst1), len(lst2)
-    for i in range(0, n):
-        for j in range(m - 1, -1, -1):
+    for i in range(len(lst1)):
+        for j in range(len(lst2)):
             if lst1[i] == lst2[j]:
                 intersection.append(lst2[j])
                 del lst2[j]
@@ -17,4 +16,3 @@ if __name__ == '__main__':
     lst2 = [2, 2, 2, 3, 5, 5, 7]
 
     print(get_intersection(lst1, lst2))
-
