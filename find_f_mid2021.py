5af5323471a4fe84d3df70ac74d6b15d0d7d6258
baraahekal <ibaraahekal@gmail.com>
2023-11-15 00:19:26 +0200

    finding if there is an element in the elementth position

--- find_f_mid2021.py
+++ find_f_mid2021.py
@@ -1,22 +1,23 @@
 def find_f(A, i) -> int:
-    if not i:
+    if i == -1:
         return -1
     if A[i] == i:
         return i
     return find_f(A, i - 1)
 
 
-A = [1, 2, 3, 4, 5, 5]
+A = [0, 2, 3, 4, 5]
 print(find_f(A, len(A) - 1))
 
 """
 Time Complexity:
-Setting up the recurrence: T(n) = T(n-1) + 1, T(0) = 1
-                                = T(n-2) + 2
-                                = T(n-3) + 3
-                                = T(n-k) + k -> solve n = k
-                                = T(n-n) + n
-                                = T(0) + n
-                                = 1 + n
-                                ∈ O(n)                  
+Setting up the recurrence: 
+T(n) = T(n-1) + 1, T(0) = 1
+     = T(n-2) + 2
+     = T(n-3) + 3
+     = T(n-k) + k -> solve n = k
+     = T(n-n) + n
+     = T(0) + n
+     = 1 + n
+     ∈ O(n)                  
 """
