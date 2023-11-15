865fdf867cd086ed12c2b38de3902c63637f4683
baraahekal <ibaraahekal@gmail.com>
2023-11-15 13:31:30 +0200

    Generating power set recursively

--- gen_powerSet.py
+++ gen_powerSet.py
@@ -1,18 +1,17 @@
-def generate_power_set_reverse(input_set, index):
+def gen_power_set(input_set, index):
     if index < 0:
         return [[]]
 
-    current_set=[]
-    for sub in generate_power_set_reverse(input_set, index - 1):
-        current_set.append(sub + [input_set[index]])
-        current_set.append(sub)
+    current_set = []
+    for sub_set in gen_power_set(input_set, index - 1):
+        current_set.append(sub_set + [input_set[index]])
+        current_set.append(sub_set)
     
     return current_set
-
 
 
 if __name__ == '__main__':
     input_set = ['a', 'b', 'c']
     index = len(input_set) - 1  
-    print(generate_power_set_reverse(['a', 'b', 'c'], index))
+    print(gen_power_set(['a', 'b', 'c'], index))
 
