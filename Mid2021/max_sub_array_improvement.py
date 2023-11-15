e47afa6d23ea97b5b921d273ab51b78812887c67
baraahekal <ibaraahekal@gmail.com>
2023-11-15 14:28:54 +0200

    Solving max sub array problem with Kadane's algorithm O(n), but it's out of mid question scope

--- Mid2021/max_sub_array_improvement.py
+++ Mid2021/max_sub_array_improvement.py
@@ -1,3 +1,6 @@
+# Kadane's Algorithm
+# Just for curiosity, out of Midterm question scope
+
 def max_subarray_sum_kadane(arr):
     cur_sum, max_sum = arr[0], arr[0]
     for i in range(1, len(arr)):
