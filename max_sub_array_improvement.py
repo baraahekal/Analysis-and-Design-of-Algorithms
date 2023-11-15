5867b477be7a2e0509a0f2f3697e56567db8577f
baraahekal <ibaraahekal@gmail.com>
2023-11-15 14:02:51 +0200

    removed unwanted files

--- max_sub_array_improvement.py
+++
@@ -1,6 +0,0 @@
-def max_subarray_sum_kadane(arr):
-    cur_sum, max_sum = arr[0], arr[0]
-    for i in range(1, len(arr)):
-        cur_sum = max(cur_sum + arr[i], arr[i])
-        max_sum = max(max_sum, cur_sum)
-    return max_sum
