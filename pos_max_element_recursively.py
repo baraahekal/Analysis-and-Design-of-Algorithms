a95dbec678ec20d206ffb27ed69fc36c3df20cac
baraahekal <ibaraahekal@gmail.com>
2023-11-17 03:21:05 +0200

    Recursive implementation for getting position of max element in an array O(n)

--- pos_max_element_recursively.py
+++ pos_max_element_recursively.py
@@ -7,3 +7,11 @@ def pos_max_element(A, low, hi):
     max_pos_right = pos_max_element(A, mid + 1, hi)
 
     return max_pos_left if A[max_pos_left] > A[max_pos_right] else max_pos_right
+
+
+
+# complex simple more time consuming version:
+def f(A, low, hi):
+    if low == hi: return low
+    mid = (low + hi) // 2
+    return f(A, low, mid) if A[f(A, low, mid)] > A[f(A, mid + 1, hi)] else f(A, mid + 1, hi)
