0623048d1c33b25026ff340fb9e9e83870474f5c
baraahekal <ibaraahekal@gmail.com>
2023-11-22 17:48:52 +0200

    Applying recursive and iterative DP on fibonacci problem

---
+++ fib_DP.py
@@ -0,0 +1,25 @@
+# Recursive DP On) instead of O(2ⁿ) using straight forward recursion
+def fib_recursive(n):
+    global res
+    if res[n]:
+        return res[n]
+    if n <= 2:
+        res[n] = 1
+    else:
+        res[n] = fib_recursive(n - 1) + fib_recursive(n - 2)
+    return res[n]
+
+
+# Iterative DP O(n)
+def fib_iterative(n):
+    global res
+    res[1] = res[2] = 1
+    for i in range(2, n + 1):
+        res[i] = res[i - 1] + res[i - 2]
+    return res[n]
+
+
+n = int(input())
+res = [0] * (n + 1)
+print(fib_iterative(n))
+print(fib_recursive(n))
